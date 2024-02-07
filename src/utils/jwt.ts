b46af582d7ca323bb3c9dfb577fd076f009d5223
guiladg <guiladg@gmail.com>
2024-02-07 11:59:12 -0300

    Refactored

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,9 +2,11 @@ import User from '../models/user';
 import * as jwt from 'jsonwebtoken';
 import RefreshToken from '../models/refreshToken';
 import { Payload, RefreshPayload } from '../types/payload';
+import { randomBytes } from 'crypto';
+import DataBaseError from '../errors/DataBaseError';
 
-/** Returns a tuple with access refresh and control tokens. Save the second one in database, related to user. */
-export const createTokens = (user: User): [string, string, string] => {
+/** Returns a tuple with access, refresh and control tokens. Saves the second one in database, related to user. */
+export function createTokens(user: User): [string, string, string] {
 	const payload: Payload = { username: user.username, role: user.role, id: user.id };
 	// Create new refresh token
 	const newAccessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
@@ -12,9 +14,8 @@ export const createTokens = (user: User): [string, string, string] => {
 		expiresIn: Number(process.env.ACCESS_TOKEN_LIFE)
 	});
 
-	// Create new refresh token
 	// Create random string to be used as refresh token identifier
-	const token = generateRandomString(16);
+	const token = randomBytes(16).toString('base64url');
 	const refreshPayload: RefreshPayload = { idUser: user.id, token };
 	const newRefreshToken = jwt.sign(refreshPayload, process.env.REFRESH_TOKEN_SECRET, {
 		algorithm: 'HS256',
@@ -29,35 +30,33 @@ export const createTokens = (user: User): [string, string, string] => {
 
 	// Save refresh token in database
 	const refreshTokenRecord = new RefreshToken();
-	refreshTokenRecord.idUser = user.id;
+	refreshTokenRecord.user = user;
 	refreshTokenRecord.token = token;
 	refreshTokenRecord.expires = Math.round(new Date().getTime() / 1000) + Number(process.env.REFRESH_TOKEN_LIFE);
 	try {
 		refreshTokenRecord.save();
-	} catch (e) {
-		throw new Error('Database Error');
+	} catch (cause) {
+		throw new DataBaseError({ message: cause.message, cause });
 	}
 
 	return [newAccessToken, newRefreshToken, newControlToken];
-};
+}
 
-/** Remove a refresh token from the database. */
+/** Synchronously verifies the provided JWT `token` against the one defined on `type` and returns the payload or throws an error. */
+export function verifyToken<T extends 'access' | 'refresh' | 'control'>(type: T, token: string): T extends 'access' ? Payload : RefreshPayload {
+	const against = type === 'access' ? process.env.ACCESS_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET;
+	return jwt.verify(token, against, { algorithms: ['HS256'] }) as any;
+}
+
+/** Removes a refresh token from the database. */
 export const removeToken = async (refreshToken: string) => {
 	try {
-		const { idUser, token } = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET) as RefreshPayload;
-		const refreshTokenRecord = await RefreshToken.findOne({ where: { idUser, token } });
+		const { idUser, token } = verifyToken('refresh', refreshToken);
+		const refreshTokenRecord = await RefreshToken.findOne({ where: { user: { id: idUser }, token } });
 		if (refreshTokenRecord) {
 			refreshTokenRecord.remove();
 		}
 	} catch (error) {
 		// Just avoid nodejs crashing
 	}
-};
-
-/** Create a random alphanumeric string. */
-const generateRandomString = (length: number) => {
-	const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-	const randomArray = Array.from({ length }, (v, k) => chars[Math.floor(Math.random() * chars.length)]);
-	const randomString = randomArray.join('');
-	return randomString;
 };
