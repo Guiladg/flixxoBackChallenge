20b5a28291c53d664af96386a8977f05476b19bf
guiladg <guiladg@gmail.com>
2024-02-07 11:57:26 -0300

    Unhandled errors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,52 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import * as bodyParser from 'body-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import routes from './routes/';
 import path from 'path';
 import dataSource from './dataSource';
+import { unhandledError } from './middlewares/unhandledError';
 
 // Load .env file
 dotenv.config();
 
+// Initialize DB connection
 dataSource
 	.initialize()
 	.then(async () => {
-		// Start express
-		const app = express();
-
-		// Middlewares
-		app.use(cors({ origin: true, credentials: true }));
-		app.use(helmet());
-		app.use(cookieParser());
-		app.use(bodyParser.json());
-
-		// API routes (fixed for Windows compatibility)
-		app.use(path.join('/', process.env.API_ROUTE ?? '').replace('\\', '/'), routes);
-
-		// Start listening requests
-		app.listen(process.env.PORT, () => {
-			console.info(`\n***************************************************************************`);
-			console.info(`*  Server started and running.`);
-			console.info(`*  Port: ${process.env.PORT}.`);
-			console.info(`*  API Route: /${process.env.API_ROUTE}`);
-			console.info(`*  Root dir: ${__dirname}.`);
-			console.info(`***************************************************************************\n`);
-		});
+		console.error(`\n***************************************************************************`);
+		console.error('*  Data Source has been initialized');
+		console.error(`***************************************************************************\n`);
 	})
 	.catch((error) => {
 		console.error(`\n***************************************************************************`);
 		console.error('*  Error initializing database:', error);
 		console.error(`***************************************************************************\n`);
 	});
+
+// create and setup express app
+const app = express();
+
+// Basic middlewares
+app.use(cors({ origin: true, credentials: true }));
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+
+// API routes (using path.posix to prevent backslashes in URLs when running on Windows)
+const useRoutes = path.posix.join('/', process.env.API_ROUTE ?? '');
+app.use(useRoutes, routes);
+
+// Add error handling middleware
+app.use(unhandledError);
+
+// Start listening requests
+app.listen(process.env.PORT, () => {
+	console.info(`\n***************************************************************************`);
+	console.info(`*  Server started and running.`);
+	console.info(`*  Port: ${process.env.PORT}.`);
+	console.info(`*  API Route: /${process.env.API_ROUTE}`);
+	console.info(`*  Root dir: ${__dirname}.`);
+	console.info(`***************************************************************************\n`);
+});
