4fb28e0c6b33650b89f6b65e65964d461355c06b
guiladg <guiladg@gmail.com>
2024-02-07 11:58:10 -0300

    Async methods

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,4 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, VirtualColumn } from 'typeorm';
-import { Length, IsNotEmpty } from 'class-validator';
 import * as bcrypt from 'bcryptjs';
 
 export const userRoles = ['admin'] as const;
@@ -12,37 +11,31 @@ export default class User extends BaseEntity {
 	id: number;
 
 	@Column()
-	@IsNotEmpty()
 	firstName: string;
 
 	@Column()
-	@IsNotEmpty()
 	lastName: string;
 
 	@VirtualColumn({ query: (alias) => `CONCAT(${alias}."lastName", ', ', ${alias}."firstName")` })
 	fullName: string;
 
-	@Column()
-	@Length(4, 20)
+	@Column({ nullable: false, length: 20 })
 	username: string;
 
-	@Column()
-	@Length(4, 100)
+	@Column({ nullable: false })
 	password: string;
 
-	@Column()
-	@IsNotEmpty()
+	@Column({ nullable: false, type: 'enum', enum: userRoles })
 	role: UserRole;
 
-	@Column()
-	@Length(3, 320)
+	@Column({ nullable: false, length: 320 })
 	email: string;
 
-	hashPassword() {
-		this.password = bcrypt.hashSync(this.password, 8);
+	async hashPassword(): Promise<void> {
+		this.password = await bcrypt.hash(this.password, 8);
 	}
 
-	checkPassword(unencryptedPassword: string) {
-		return bcrypt.compareSync(unencryptedPassword, this.password);
+	async checkPassword(unencryptedPassword: string): Promise<boolean> {
+		return bcrypt.compare(unencryptedPassword, this.password);
 	}
 }
