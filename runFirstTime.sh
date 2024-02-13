fda379d951e44df131c0920a72e6b90e5f8af7bd
guiladg <guiladg@gmail.com>
2024-02-13 14:35:03 -0300

    Minor changes

--- runFirstTime.sh
+++ runFirstTime.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+echo "Checking for first run..."
+
 # Checks if a file named CONTAINER_FIRST_STARTUP does not exists
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+   echo "First run detected. Initializing..."
    # Creates the file (after a restart this block will be ommiited)
    touch /$CONTAINER_FIRST_STARTUP
    # Run mock DB data process and then start the app
-   npm run mockData:prod && npm run start
+   echo "Mocking data on DB..."
+   npm run mockData:prod
+   echo "Runing application..."
+   npm run start
 else
    # Just start the app
+   echo "First run already happened. Restarting..."
+   echo "Runing application..."
    npm run start
 fi
